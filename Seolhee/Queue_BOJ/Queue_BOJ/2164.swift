6f90f6fd880e8fdff277375504c0c5b272bcecb0
김설희 <2750seolhee@naver.com>
2021-08-13 22:10:16 +0900

    BOJ #2164 카드2 문제 풀이

---
+++ Seolhee/Queue_BOJ/Queue_BOJ/2164.swift
@@ -0,0 +1,19 @@
+// BOJ #2164 카드2
+
+import Foundation
+
+let N = Int(readLine()!)!
+var queue = Array(1...N)
+var num = 1
+var tmp = 0
+
+if N == 1 { print(1) }
+else {
+    while true {
+        queue[tmp] = 0
+        queue.append(queue[tmp + 1])
+        queue[tmp + 1] = 0
+        if queue[queue.count - 2] == 0 { print(queue.last!); break }
+        tmp += 2
+    }
+}
