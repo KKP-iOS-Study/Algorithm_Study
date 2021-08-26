37ab6c3cc8a690ace6a31ddde1cf38ab12465ff2
김설희 <2750seolhee@naver.com>
2021-08-26 22:54:17 +0900

    no message

--- Seolhee/Greedy_BOJ/Greedy_BOJ/main.swift
+++ Seolhee/Greedy_BOJ/Greedy_BOJ/main.swift
@@ -1,18 +1,18 @@
 // BOJ #7570 줄 세우기
 //
 // LIS 알고리즘 공부하고. 다시 풀어보자..
-
-import Foundation
-
-let n = Int(readLine()!)!
-var nums = readLine()!.split(separator: " ").map({Int(String($0))!})
-var arr1 = [nums[0]]
-var arr2 = [Int]()
-
-for i in 1..<n {
-    if arr1.last! < nums[i] { arr1.append(nums[i]) }
-    else { arr2.append(nums[i]) }
-}
-
-arr1.count > arr2.count ? print(n - arr1.count) : print(n - arr2.count)
+//
+//import Foundation
+//
+//let n = Int(readLine()!)!
+//var nums = readLine()!.split(separator: " ").map({Int(String($0))!})
+//var arr1 = [nums[0]]
+//var arr2 = [Int]()
+//
+//for i in 1..<n {
+//    if arr1.last! < nums[i] { arr1.append(nums[i]) }
+//    else { arr2.append(nums[i]) }
+//}
+//
+//arr1.count > arr2.count ? print(n - arr1.count) : print(n - arr2.count)
 
