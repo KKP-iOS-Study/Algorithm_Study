e0e94afac59c12606c3532cfa484c54d6af13639
김설희 <2750seolhee@naver.com>
2021-08-28 21:13:31 +0900

    BOJ #1965 문제 풀이

---
+++ Seolhee/Greedy_BOJ/Greedy_BOJ/1965.swift
@@ -0,0 +1,22 @@
+// BOJ #1965 상자넣기
+//
+// LIS 예제로 풀어봄
+
+import Foundation
+
+var n = Int(readLine()!)!
+var box = readLine()!.split(separator: " ").map({Int(String($0))!})
+var count = [Int]()
+var max = 1
+
+for i in 0..<n {
+    count.append(1)
+    
+    for j in 0..<i {
+        if box[j] < box[i] && count[i] < count[j]+1 { count[i] = count[j]+1 }
+    }
+    
+    if max < count[i] { max = count[i] }
+}
+
+print(max)
