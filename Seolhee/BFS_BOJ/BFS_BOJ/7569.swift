ca3060829b3d121b6285798cdbd6e63ad9cc38e7
김설희 <2750seolhee@naver.com>
2021-10-07 23:54:54 +0900

    #7569 토마토 설명 추가

--- Seolhee/BFS_BOJ/BFS_BOJ/7569.swift
+++ Seolhee/BFS_BOJ/BFS_BOJ/7569.swift
@@ -32,12 +32,13 @@ func bfs() {
             let nx = x + dx[j]
             let ny = y + dy[j]
             
-            if nz < 0 || nx < 0 || ny < 0 || nz >= h || nx >= n || ny >= m { continue }
+            if nz < 0 || nx < 0 || ny < 0 || nz >= h || nx >= n || ny >= m { continue } // 예외
             
+            // 탐색한 자리가 0일 경우! (이제 익혀질 것임)
             if box[nz][nx][ny] == 0 {
-                queue.append((nz, nx, ny))
-                box[nz][nx][ny] = box[z][x][y] + 1
-                count += 1
+                queue.append((nz, nx, ny)) // 1로 익혀졌으므로, 새로운 탐색 시작점이 추가되어야 함
+                box[nz][nx][ny] = box[z][x][y] + 1 // 이 자리까지 익혀지는데에 걸린 시간을 누적
+                count += 1 // 익힌 카운트 누적
             }
         }
     }
@@ -46,12 +47,15 @@ func bfs() {
 for z in 0..<h {
     for x in 0..<n {
         for y in 0..<m {
-            if box[z][x][y] == 1 { queue.append((z, x, y)) }
-            else if box[z][x][y] == 0 { empty += 1 }
+            if box[z][x][y] == 1 { queue.append((z, x, y)) } // 익은 토마토 위치를 탐색 시작점으로 추가
+            else if box[z][x][y] == 0 { empty += 1 } // 안익은 토마토의 개수 세기
         }
     }
 }
 
 bfs()
 
+// empty가 0이면 처음부터 안익은 토마토가 없었다는 뜻이므로 바로 0 출력,
+// empty가 count와 같아졌다면 익혀지는데 걸린 누적 시간들 중 가장 큰 수 출력,
+// empty가 count와 같아지지 못했다면 아직 안익은 토마토가 있다는 뜻이므로 -1 출력
 empty == 0 ? print(0) : (empty == count ? print(box.flatMap({$0}).flatMap({$0}).max()!-1) : print(-1))
