9598d663daa01cf968136dd33574c30004944308
HoSeong <camosss@naver.com>
2021-10-19 15:46:07 +0900

    1149 RGB거리

--- Hosung/DP_BOJ/DP_BOJ/1149 RGB거리.swift
+++ Hosung/DP_BOJ/DP_BOJ/1149 RGB거리.swift
@@ -22,8 +22,8 @@ dp[0][2] = rgb[0][2]
 
 for i in 1..<n {
     dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + rgb[i][0]
-    dp[i][1] += min(dp[i-1][0], dp[i-1][2]) + rgb[i][1]
-    dp[i][2] += min(dp[i-1][0], dp[i-1][1]) + rgb[i][2]
+    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + rgb[i][1]
+    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + rgb[i][2]
 }
 
 print(dp.last!.min()!)
