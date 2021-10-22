d8c48fcead0e2731c5a8e5bf7cb2a25ba6137baf
HoSeong <camosss@naver.com>
2021-10-22 14:38:57 +0900

    9461 파도반 수열

--- Hosung/DP_BOJ/DP_BOJ/9461 파도반 수열.swift
+++ Hosung/DP_BOJ/DP_BOJ/9461 파도반 수열.swift
@@ -10,16 +10,15 @@
 let test = Int(readLine()!)!
 
 var p = [Int](repeating: 0, count: 100)
-
 p[0...4] = [1, 1, 1, 2, 2]
+
+
+for i in 5..<100 {
+    p[i] = p[i-1] + p[i-5]
+}
 
 for _ in 0..<test {
     let n = Int(readLine()!)!-1
-
-    for i in 5...n {
-        p[i] = p[i-1] + p[i-5]
-    }
-
     print(p[n])
 }
 
---
+++ Hosung/DP_BOJ/DP_BOJ/main.swift
@@ -0,0 +1,7 @@
+//
+//  main.swift
+//  DP_BOJ
+//
+//  Created by 강호성 on 2021/10/22.
+//
+
