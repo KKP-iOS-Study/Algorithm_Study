d4b17f9ec0347321baf846706bb5b19f89031b37
HoSeong <camosss@naver.com>
2021-11-29 23:56:41 +0900

    30

--- Hosung/Greedy2_BOJ/Greedy2_BOJ/main.swift
+++ Hosung/Greedy2_BOJ/Greedy2_BOJ/main.swift
@@ -5,14 +5,18 @@
 //  Created by 강호성 on 2021/11/29.
 //
 
-let N = Int(readLine()!)!
+let N = readLine()!.sorted(by: >)
 
-// 30의 배수 = 30으로 나눴을 때 나머지가 0
-// 주어진 N에서 숫자 조합
-// 모든 숫자조합을 찾으면 당연히 시간초과
-// 30의 배수이면 맨뒷자리는 무조건 0
-// while문으로 모든 숫자중 제일 큰수부터 나열해서 대입했을때 30의 배수이면 break
+let arr = N.map{Int(String($0))!}
+let arrSum = arr.reduce(0, +)
 
-print(String(N))
+var ans = ""
 
-
+if arrSum % 3 == 0 && N.contains("0") {
+    for i in N {
+        ans += String(i)
+    }
+    print(ans)
+} else {
+    print(-1)
+}
