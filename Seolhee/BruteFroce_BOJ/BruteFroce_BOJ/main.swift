841270c115aaa0cf2ae6f6d63a63a9028c24c936
김설희 <2750seolhee@naver.com>
2022-01-19 00:49:10 +0900

    14500 테트로미노 문제 풀이

--- Seolhee/BruteFroce_BOJ/BruteFroce_BOJ/main.swift
+++ Seolhee/BruteFroce_BOJ/BruteFroce_BOJ/main.swift
@@ -1,60 +1,37 @@
 // BOJ 14500 테트로미노
-/*
-let input = readLine()!.split(separator: " ").map({Int(String($0))!})
+
+let input = readLine()!.split(separator: " ").map{Int(String($0))!}
 let n = input[0]
 let m = input[1]
 var paper = [[Int]]()
-var check = [[Bool]](repeating: [Bool](repeating: false, count: m), count: n)
 var result = 0
 
-for _ in 0..<n {
-    paper.append(readLine()!.split(separator: " ").map({Int(String($0))!}))
-}
+for _ in 0..<n { paper.append(readLine()!.split(separator: " ").map{Int(String($0))!}) }
 
-let d: [(x: Int, y: Int)] = [(-1, 0), (0, -1), (1, 0), (0, 1)]
-let d2: [[(x: Int, y: Int)]] = [[(0, 0), (0, 1), (0, 2), (1, 1)],
-          [(0, 0), (1, 0), (2, 0), (1, 1)],
-          [(0, 0), (0, 1), (0, 2), (-1, 1)],
-          [(0, 0), (0, 1), (-1, 1), (1, 1)]]
-
-func dfs(_ x: Int, _ y: Int, _ count: Int, _ sum: Int) {
+func check(_ x: Int, _ y :Int, _ before: Int, _ count: Int, _ sum: Int) {
     if count == 4 { result = max(result, sum); return }
-    
-    for i in 0..<4 {
-        let nx = x + d[i].x
-        let ny = y + d[i].y
-        guard 0 <= nx && nx < n && 0 <= ny && ny < m else { continue }
-        if check[nx][ny] == true { break }
-        check[nx][ny] = true
-        dfs(nx, ny, count+1, sum+paper[nx][ny])
-        check[nx][ny] = false
-    }
+
+    if x-1 >= 0 && before != 1 { check(x-1, y, 2, count+1, sum+paper[x-1][y]) }
+    if x+1 < n && before != 2 { check(x+1, y, 1, count+1, sum+paper[x+1][y]) }
+    if y-1 >= 0 && before != 3 { check(x, y-1, 4, count+1, sum+paper[x][y-1]) }
+    if y+1 < m && before != 4 { check(x, y+1, 3, count+1, sum+paper[x][y+1]) }
 }
 
 func checkSpecial(_ x: Int, _ y: Int) {
-    for i in 0..<4 {
-        var sum = 0
-        var success = true
-        
-        for j in 0..<4 {
-            let nx = x + d2[i][j].x
-            let ny = y + d2[i][j].y
-            guard 0 <= nx && nx < n && 0 < ny && ny < m else { success = false; break }
-            sum += paper[nx][ny]
-        }
-        
-        if success == true { result = max(result, sum) }
+    if x+1 < n && y-1 >= 0 && y+1 < m {
+        result = max(result, max(paper[x][y] + paper[x+1][y-1] + paper[x+1][y] + paper[x+1][y+1], paper[x][y] + paper[x][y-1] + paper[x+1][y] + paper[x][y+1]))
+    }
+
+    if x+2 < n && y+1 < m {
+        result = max(result, max(paper[x][y] + paper[x+1][y] + paper[x+1][y+1] + paper[x+2][y], paper[x+1][y] + paper[x][y+1] + paper[x+1][y+1] + paper[x+2][y+1]))
     }
 }
 
-for x in 0..<n {
-    for y in 0..<m {
-        check[x][y] = true
-        dfs(x, y, 0, 0)
-        check[x][y] = false
-        checkSpecial(x, y)
+for i in 0..<n {
+    for j in 0..<m {
+        check(i, j, 0, 1, paper[i][j])
+        checkSpecial(i, j)
     }
 }
 
 print(result)
-*/
