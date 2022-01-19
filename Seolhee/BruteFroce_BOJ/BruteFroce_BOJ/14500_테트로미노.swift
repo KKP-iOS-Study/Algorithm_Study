9edc29500f95231442ec38cca0e820517ef9685d
김설희 <2750seolhee@naver.com>
2022-01-19 14:52:57 +0900

    14500 테트로미노 문제 풀이

--- Seolhee/BruteFroce_BOJ/BruteFroce_BOJ/14500_테트로미노.swift
+++ Seolhee/BruteFroce_BOJ/BruteFroce_BOJ/14500_테트로미노.swift
@@ -1,4 +1,8 @@
 // BOJ 14500 테트로미노
+//
+// 'ㅏ' 형태의 경우만 예외로 하고, 나머지 경우는 dp로 풀어주었음 (dfs로 했을 때 시간 초과)
+// dp는 check 함수의 'before' 파라미터를 통해서 중복을 피해주어 구현
+// 'ㅏ'만 예외로 빼 준 이유는 'ㅏ' 모양의 경우 중복을 제외할 수 없기 때문
 
 let input = readLine()!.split(separator: " ").map{Int(String($0))!}
 let n = input[0]
@@ -18,10 +22,12 @@ func check(_ x: Int, _ y :Int, _ before: Int, _ count: Int, _ sum: Int) {
 }
 
 func checkSpecial(_ x: Int, _ y: Int) {
+    // 'ㅗ', 'ㅜ'
     if x+1 < n && y-1 >= 0 && y+1 < m {
         result = max(result, max(paper[x][y] + paper[x+1][y-1] + paper[x+1][y] + paper[x+1][y+1], paper[x][y] + paper[x][y-1] + paper[x+1][y] + paper[x][y+1]))
     }
-
+    
+    // 'ㅏ', 'ㅓ'
     if x+2 < n && y+1 < m {
         result = max(result, max(paper[x][y] + paper[x+1][y] + paper[x+1][y+1] + paper[x+2][y], paper[x+1][y] + paper[x][y+1] + paper[x+1][y+1] + paper[x+2][y+1]))
     }
